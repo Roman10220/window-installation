17524133f73420fd34992c5dda61f825754f9939
Umk_a <03orv15@gmail.com>
2023-03-14 21:52:45 +0200

    Made tabi at the company section

--- js/script.js
+++ js/script.js
@@ -22,5 +22,31 @@ function onItemClick(item) {
 		}
 	});
 }
-
 document.querySelector('.menu__item').click();
+/*-------------------------------------------------------*/
+const companyMenuItem = document.querySelectorAll('.company__menu-item');
+const companyBodyContent = document.querySelectorAll('.company__body-content');
+
+companyMenuItem.forEach(companyOnItemClick);
+
+function companyOnItemClick(item) {
+	item.addEventListener('click', function() {
+		let currentItem = item;
+		let tabId = currentItem.getAttribute('data-tabTwo');
+		let currentTab = document.querySelector(tabId);
+
+		if (!currentItem.classList.contains('active')) {
+			companyMenuItem.forEach(function(item) {
+				item.classList.remove('active');
+			});
+			companyBodyContent.forEach(function(item) {
+				item.classList.remove('active')
+			});
+
+			
+			currentItem.classList.add('active');
+			currentTab.classList.add('active');
+		}
+	});
+}
+document.querySelector('.company__menu-item').click();
